1cdcc30e93943bc32a802dee957b20e3158c2eb5
Umar Alfaruq <umaralfaar@gmail.com>
2023-03-01 15:56:40 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-trailing-spaces
+const { 
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  // eslint-disable-next-line comma-dangle
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+// eslint-disable-next-line eol-last
+module.exports = routes;
